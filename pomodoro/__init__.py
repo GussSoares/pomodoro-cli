dabcf4597794db2050e18e57295e3aedc80182ca
Gustavo Soares <gustavo.soares.cdc@gmail.com>
2025-03-07 22:24:24 -0300

    feature: add pause; add restore; add help

--- pomodoro/__init__.py
+++ pomodoro/__init__.py
@@ -1,19 +1,38 @@
 import curses
+import os
 import sys
 import time
+import pathlib
+import configparser
 
 import pyfiglet
 
 from pomodoro.constants import Constants, Messages
 
 
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 class Pomodoro:
-    def __init__(self):
+    def __init__(self, restore: bool = False, mute: bool = False):
         self.pause = False
+        self.rest = False
         self.minute = 0
         self.second = 0
+        self.loop = 0
         self.window = curses.initscr()
         self.window.nodelay(True)
+
+        self.config_file_path = os.path.join(PACKAGE_DIR, 'settings.ini')
+
+        self.init_config_file()
+
+        if restore:
+            config = self.get_config()
+            self.pause = eval(config['Settings']['pause'])
+            self.rest = eval(config['Settings']['rest'])
+            self.minute = eval(config['Settings']['minute'])
+            self.second = eval(config['Settings']['second'])
 
         curses.start_color()
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -30,25 +49,75 @@ class Pomodoro:
 
     @property
     def info_message(self):
-        return Messages.PAUSE.value if self.pause else Messages.WORK.value
+        if self.pause:
+            return Messages.PAUSE.value
+        if self.rest:
+            return Messages.REST.value
+        return Messages.WORK.value
 
-    def update_clock(self, pause: bool = False):
+    def init_config_file(self):
+        if not pathlib.Path(self.config_file_path).exists():
+            with open(self.config_file_path, 'w'):
+                self.set_config()
+
+    def get_config(self) -> dict:
+        config = configparser.ConfigParser()
+        config.read(self.config_file_path)
+        return config
+
+    def set_config(self) -> None:
+        with open(self.config_file_path, 'w') as configfile:
+            config = configparser.ConfigParser()
+            config['Settings'] = {
+                'pause': self.pause,
+                'rest': self.rest,
+                'minute': self.minute,
+                'second': self.second,
+                'loop': self.loop,
+            }
+            config.write(configfile)
+
+    def save(self) -> None:
+        self.set_config()
+
+    def update_clock(self):
+        long_rest = False
+        
+        if self.pause:
+            return
+
         if self.second < 59:
             self.second += 1
         else:
             self.minute += 1
             self.second = 0
 
-        if self.second == Constants.MINUTES_TO_PAUSE.value or self.pause:
+        if self.loop == 4:
+            long_rest = True
+
+        if self.minute == Constants.MINUTES_TO_WORK.value:
             self.minute = 0
             self.second = 0
-            self.pause = not self.pause
+            self.rest = True
+
+        if self.rest and self.minute == Constants.MINUTES_TO_REST.value and not long_rest:
+            self.minute = 0
+            self.second = 0
+            self.rest = False
+            self.loop += 1
+
+        elif self.rest and self.minute == Constants.MINUTES_TO_LONG_REST.value and long_rest:
+            self.minute = 0
+            self.second = 0
+            self.rest = False
+            self.loop =  0
+            long_rest = False
 
     def get_center_xpos(self, text_length: int, max_x: int = None) -> int:
         return int(((max_x or self.max_x) // 2) - (text_length // 2) - (text_length % 2))
 
     def get_center_ypos(self, text_height: int, max_y: int = None) -> int:
-        return int(((max_y or self.max_y) // 2) + (text_height // 2) + (text_height % 2))
+        return int(((max_y or self.max_y) // 2) + (text_height // 2) - (text_height % 2))
 
     def write_work_message(self):
         self.window.addstr(
@@ -77,12 +146,12 @@ class Pomodoro:
     def main(self, _):
         while True:
             k = self.window.getch()
-            quick_pause = False
 
             if k == ord('q'):
+                self.save()
                 sys.exit()
             if k == ord('p'):
-                quick_pause = not quick_pause
+                self.pause = not self.pause
 
             self.window.clear()
             self.init_panel()
@@ -96,7 +165,7 @@ class Pomodoro:
 
             self.window.addstr(aux_y, self.get_center_xpos(self.info_message.__len__()), self.info_message)
 
-            self.update_clock(pause=quick_pause)
+            self.update_clock()
 
             self.window.refresh()
             time.sleep(1)
